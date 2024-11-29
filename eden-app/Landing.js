26b01df1ce11139d9d7db0110d1c24aed2cb1261
VEMA <144062503+vemadevelopers@users.noreply.github.com>
2024-11-30 01:20:26 +0300

    Update and rename js to Landing.js

---
+++ eden-app/Landing.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.getElementById("logo-container").style.display = "none";
+        document.getElementById("button-container").style.display = "block";
+    }, 3000); // 3 seconds loading time
+});
+
